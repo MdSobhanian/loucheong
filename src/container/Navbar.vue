bf35a1dded08c44ca0db6e63f357846ea2180bac
MdSobhanian <jashims999@gmail.com>
2020-04-16 12:14:32 +0800

    Initial commit

---
+++ src/container/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    navbar
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../view/Home'
+import Navbar from "../container/Navbar";
+import Project from '../view/Project'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Navbar,
+      children:[
+        {
+          path: '/',
+          component: Home,
+        },
+        {
+          path: '/project',
+          component: Project,
+        }
+      ]
+    }
+  ]
+})
